3a19fafdcc9ca8cb3a02f48da74ae6dece875f2d
Luisa Diniz Lopes <Luisa@MacBook-Pro-de-Paulo.local>
2022-07-11 11:57:56 -0300

    WIP: relational db tests

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/luisadiniz/homecontroller
 
 go 1.18
 
-require github.com/lib/pq v1.10.6 // indirect
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	github.com/moemoe89/go-unit-test-sql v0.0.0-20200619083906-541278f731fe // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- repositories/relational_test.go
+++ repositories/relational_test.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"log"
 	"reflect"
 	"testing"
 
-	_ "github.com/lib/pq"
+	"github.com/DATA-DOG/go-sqlmock"
+	r "github.com/moemoe89/go-unit-test-sql/repository"
+	"github.com/stretchr/testify/assert"
 )
 
 type databaseEngineMock struct {
@@ -48,6 +51,7 @@ func TestRelationalRepository_Get(t *testing.T) {
 			name: "IfDatabaseReturnError",
 			fields: fields{
 				data: &databaseEngineMock{
+					rows:     nil,
 					queryErr: errors.New("Data base fail"),
 				},
 			},
@@ -72,5 +76,110 @@ func TestRelationalRepository_Get(t *testing.T) {
 				t.Errorf("RelationalRepository.Get() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+var u = &r.UserModel{
+	Name: "sala",
+	IsOn: true,
+}
+
+func NewMock() (*sql.DB, sqlmock.Sqlmock) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	return db, mock
+}
+
+func TestGetByID_IfHasRowReturnValue(t *testing.T) {
+	db, mock := NewMock()
+	repo := &RelationalRepository{db}
+
+	query := "SELECT name, isOn FROM lighbulbs WHERE name=$1"
+
+	rows := sqlmock.NewRows([]string{"name", "isOn"}).
+		AddRow(u.Name, u.IsOn)
+
+	mock.ExpectQuery(query).WithArgs(u.Name).WillReturnRows(rows)
+
+	lightbulb, err := repo.GetById(context.Background(), u.Name)
+	assert.NotNil(t, lightbulb)
+	assert.NoError(t, err)
+
+	want := true
+
+	if !reflect.DeepEqual(lightbulb, want) {
+		println("RelationalRepository.GetById() = %v, want %v", lightbulb, want)
+	}
+}
+func TestCreate_IfItWorksReturnNoError(t *testing.T) {
+	db, mock := NewMock()
+	repo := &RelationalRepository{db}
+
+	query := "INSERT INTO lightbulbs (name, isOn) VALUES ($1, $2)"
+
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WithArgs("cozinha", true).WillReturnResult(sqlmock.NewResult(0, 1))
+
+	err := repo.Create(context.Background(), "cozinha", true)
+	if err != nil {
+		println("RelationalRepository.Create() error = %v", err)
+		return
+	}
+
+	want := map[string]bool{
+		"cozinha": true,
+	}
+
+	if !reflect.DeepEqual(mock, want) {
+		println("RelationalRepository.Create() = %v, want %v", mock, want)
+	}
+}
+func TestUpdate_IfItWorksReturnNoError(t *testing.T) {
+	db, mock := NewMock()
+	repo := &RelationalRepository{db}
+
+	mock.NewRows([]string{"name", "isOn"}).
+		AddRow("cozinha", true)
+
+	query := "UPDATE lightbulbs SET isOn=$2 WHERE name=$1;"
+
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WithArgs("cozinha", false).WillReturnResult(sqlmock.NewResult(0, 1))
+
+	err := repo.Update(context.Background(), "cozinha", false)
+	println("RelationalRepository.Update() error = %v", err)
+
+	want := map[string]bool{
+		"cozinha": false,
+	}
+
+	if !reflect.DeepEqual(mock, want) {
+		println("RelationalRepository.Update() = %v, want %v", mock, want)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	db, mock := NewMock()
+	repo := &RelationalRepository{db}
+
+	mock.NewRows([]string{"name", "isOn"}).
+		AddRow("sala", true).
+		AddRow("cozinha", false)
+
+	query := "DELETE FROM lightbulbs WHERE name=sala"
+
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WillReturnResult(sqlmock.NewResult(0, 1))
+
+	err := repo.Delete(context.Background(), "quarto")
+	println("RelationalRepository.Detele() error = %v", err)
+
+	want := map[string]bool{}
+
+	if !reflect.DeepEqual(mock, want) {
+		println("RelationalRepository.Delete() = %v, want %v", mock, want)
 	}
 }
