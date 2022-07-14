02dd1cc435333db4677a123c355e10d691bd0add
Luisa Diniz Lopes <Luisa@MacBook-Pro-de-Paulo.local>
2022-07-13 21:17:48 -0300

    feat: relational db tests all done

--- repositories/relational_test.go
+++ repositories/relational_test.go
@@ -142,7 +142,7 @@ func TestRelationalRepository_GetById(t *testing.T) {
 			wantErr: true,
 		},
 		{
-			name: "WhenDatabaseReturnHasRow_ShouldReturnBoolValueAndNoError",
+			name: "WhenDatabaseUpdateValue_ShouldReturnNoError",
 			fields: fields{
 				data: func() DatabaseEngine {
 					db, mock, _ := sqlmock.New()
@@ -176,6 +176,211 @@ func TestRelationalRepository_GetById(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("RelationalRepository.GetById() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRelationalRepository_Delete(t *testing.T) {
+	type fields struct {
+		data func() DatabaseEngine
+	}
+	type args struct {
+		ctx  context.Context
+		name string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "IfDatabaseReturnError",
+			fields: fields{
+				data: func() DatabaseEngine {
+					db, mock, _ := sqlmock.New()
+
+					mock.ExpectExec(regexp.QuoteMeta("DELETE FROM lightbulbs WHERE name=$1")).WithArgs("sala").WillReturnError(errors.New("Delete failed!"))
+					return db
+				},
+			},
+			args: args{
+				ctx:  context.Background(),
+				name: "sala",
+			},
+			wantErr: true,
+		},
+		{
+			name: "WhenDatabaseDeletesValue_ShouldReturnNoError",
+			fields: fields{
+				data: func() DatabaseEngine {
+					db, mock, _ := sqlmock.New()
+
+					rows := mock.NewRows([]string{
+						"name", "isOn",
+					})
+					rows.AddRow("cozinha", true)
+
+					mock.ExpectExec(regexp.QuoteMeta("DELETE FROM lightbulbs WHERE name=$1")).WithArgs("cozinha").WillReturnResult(sqlmock.NewResult(0, 0))
+
+					return db
+				},
+			},
+			args: args{
+				ctx:  context.Background(),
+				name: "cozinha",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &RelationalRepository{
+				data: tt.fields.data(),
+			}
+			if err := l.Delete(tt.args.ctx, tt.args.name); (err != nil) != tt.wantErr {
+				t.Errorf("RelationalRepository.Delete() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestRelationalRepository_Create(t *testing.T) {
+	type fields struct {
+		data func() DatabaseEngine
+	}
+	type args struct {
+		ctx   context.Context
+		name  string
+		value bool
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "IfDatabaseReturnError",
+			fields: fields{
+				data: func() DatabaseEngine {
+					db, mock, _ := sqlmock.New()
+
+					mock.ExpectExec(regexp.QuoteMeta("INSERT INTO lightbulbs (name, isOn) VALUES ($1, $2)")).WithArgs("quarto", false).WillReturnError(errors.New("Creation failed!"))
+					return db
+				},
+			},
+			args: args{
+				ctx:   context.Background(),
+				name:  "quarto",
+				value: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "WhenDatabaseCreateNewEntry_ShouldReturnNoError",
+			fields: fields{
+				data: func() DatabaseEngine {
+					db, mock, _ := sqlmock.New()
+
+					rows := mock.NewRows([]string{
+						"name", "isOn",
+					})
+					rows.AddRow("cozinha", true)
+
+					mock.ExpectExec(regexp.QuoteMeta("INSERT INTO lightbulbs (name, isOn) VALUES ($1, $2)")).WithArgs("quarto", true).WillReturnResult(sqlmock.NewResult(0, 0))
+
+					return db
+				},
+			},
+			args: args{
+				ctx:   context.Background(),
+				name:  "quarto",
+				value: true,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &RelationalRepository{
+				data: tt.fields.data(),
+			}
+			if err := l.Create(tt.args.ctx, tt.args.name, tt.args.value); (err != nil) != tt.wantErr {
+				t.Errorf("RelationalRepository.Create() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestRelationalRepository_Update(t *testing.T) {
+	type fields struct {
+		data func() DatabaseEngine
+	}
+	type args struct {
+		ctx   context.Context
+		name  string
+		value bool
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "IfDatabaseReturnError",
+			fields: fields{
+				data: func() DatabaseEngine {
+					db, mock, _ := sqlmock.New()
+					rows := mock.NewRows([]string{
+						"name", "isOn",
+					})
+					rows.AddRow("quarto", true)
+
+					mock.ExpectExec(regexp.QuoteMeta("UPDATE lightbulbs SET isOn=$2 WHERE name=$1;")).WithArgs("quarto", false).WillReturnError(errors.New("Update Failed!"))
+					return db
+				},
+			},
+			args: args{
+				ctx:   context.Background(),
+				name:  "quarto",
+				value: false,
+			},
+			wantErr: true,
+		},
+		{
+			name: "WhenDatabaseUpdatesValue_ShouldReturnNoError",
+			fields: fields{
+				data: func() DatabaseEngine {
+					db, mock, _ := sqlmock.New()
+
+					rows := mock.NewRows([]string{
+						"name", "isOn",
+					})
+					rows.AddRow("quarto", true)
+
+					mock.ExpectExec(regexp.QuoteMeta("UPDATE lightbulbs SET isOn=$2 WHERE name=$1;")).WithArgs("quarto", false).WillReturnResult(sqlmock.NewResult(0, 0))
+
+					return db
+				},
+			},
+			args: args{
+				ctx:   context.Background(),
+				name:  "quarto",
+				value: false,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &RelationalRepository{
+				data: tt.fields.data(),
+			}
+			if err := l.Update(tt.args.ctx, tt.args.name, tt.args.value); (err != nil) != tt.wantErr {
+				t.Errorf("RelationalRepository.Update() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
